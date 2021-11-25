d79e597aaeb5d6747f081b292175ec75d0cf7afe
Christian Eiden <christian@eiden.ch>
2021-11-25 07:33:51 +0100

    Dactyl CC Settings
    Signed-off-by: Christian Eiden <christian@eiden.ch>

---
+++ keyboards/handwired/dactyl_cc_promicro/rules.mk
@@ -0,0 +1,20 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build Options
+BOOTMAGIC_ENABLE = lite     # Enable Bootmagic Lite
+EXTRAKEY_ENABLE  = yes
+COMMAND_ENABLE   = yes
+MOUSEKEY_ENABLE  = no
+LTO_ENABLE       = no
+NKRO_ENABLE      = yes
+CONSOLE_ENABLE   = no         # Console for debug
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE  = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE     = no           # Audio output
+SPLIT_KEYBOARD   = yes
+
