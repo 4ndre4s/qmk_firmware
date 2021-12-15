07f734893576953e9de34e82aa15e1c800ea7daf
Andreas Partsch <public@partsch.xyz>
2021-12-15 21:22:35 +0100

    Disable autoshift

--- keyboards/handwired/dactyl_cc_promicro/rules.mk
+++ keyboards/handwired/dactyl_cc_promicro/rules.mk
@@ -17,5 +17,5 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE  = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE     = no           # Audio output
 SPLIT_KEYBOARD   = yes
-AUTO_SHIFT_ENABLE = yes
+AUTO_SHIFT_ENABLE = no
 TAP_DANCE_ENABLE = yes
