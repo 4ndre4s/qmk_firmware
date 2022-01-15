f9d3c1fbb480e3642b356fa21ff602f6dd9f86b7
Andreas Partsch <public@partsch.xyz>
2022-01-15 20:42:33 +0100

    flip z and y

--- keyboards/handwired/dactyl_cc_promicro/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_cc_promicro/keymaps/default/keymap.c
@@ -36,11 +36,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * .-----------------------------------------.                                  .-----------------------------------------.
    * | GEsc |   1  |   2  |   3  |   4  |   5  |                                  |   6  |   7  |   8  |   9  |   0  |  / ? |
    * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
-   * | Tab  |  ' " |  , < |  . > |   P  |   Y  |                                  |   F  |   G  |   C  |   R  |   L  |  = + |
+   * | Tab  |  ' " |  , < |  . > |   P  |   Z  |                                  |   F  |   G  |   C  |   R  |   L  |  = + |
    * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
    * | TEXTW|   A  |   O  |   E  |   U  |   I  |                                  |   D  |   H  |   T  |   N  |   S  |  - _ |
    * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
-   * | Shft |  ; : |   Q  |   J  |   K  |   X  |                                  |   B  |   M  |   W  |   V  |   Z  | Shft |
+   * | Shft |  ; : |   Q  |   J  |   K  |   X  |                                  |   B  |   M  |   W  |   V  |   Y  | Shft |
    * '-----------------------------------------'                                  '-----------------------------------------'
    *        |      |      | left | rght |     .---------------.    .---------------.     |  up  | down |      |      |
    *        |      |      |      |      |    / Ctrl  /  Alt  /      \  Alt  \  Ctrl \    |      |      |      |      |
@@ -54,9 +54,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                         '-----------------------'                      '-----------------------'
    */
    KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_SLASH,
-   KC_TAB , KC_QUOT, KC_COMM, KC_DOT , KC_P   , KC_Y   ,                                     KC_F   , KC_G   , KC_C   , KC_R   , KC_L   , KC_EQUAL,
+   KC_TAB , KC_QUOT, KC_COMM, KC_DOT , KC_P   , KC_Z   ,                                     KC_F   , KC_G   , KC_C   , KC_R   , KC_L   , KC_EQUAL,
    TEXTWIN, KC_A   , KC_O   , KC_E   , KC_U   , KC_I   ,                                     KC_D   , KC_H   , KC_T   , KC_N   , KC_S   , KC_MINS,
-   KC_LSFT, KC_SCLN, KC_Q   , KC_J   , KC_K   , KC_X   ,                                     KC_B   , KC_M   , KC_W   , KC_V   , KC_Z   , KC_RSFT,
+   KC_LSFT, KC_SCLN, KC_Q   , KC_J   , KC_K   , KC_X   ,                                     KC_B   , KC_M   , KC_W   , KC_V   , KC_Y   , KC_RSFT,
             XXXXXXX, XXXXXXX, KC_LEFT, KC_RGHT,                                                       KC_UP  , KC_DOWN, XXXXXXX, XXXXXXX,
                                                      KC_LCTL, KC_LALT,         KC_RALT, KC_RCTL,
                                                               KC_HOME,         KC_END ,
